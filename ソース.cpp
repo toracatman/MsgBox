94bc808ec9efb073d675d7a39f8229374c838da5
toracatman <42952643+toracatman@users.noreply.github.com>
2024-10-07 18:34:54 +0900

    Update

--- ソース.cpp
+++ ソース.cpp
@@ -1,70 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <io.h>
+#include <fcntl.h>
+#include <stdio.h>
 #include <Windows.h>
 #include <wchar.h>
 
-
-wchar_t s[256], t[256];
+wchar_t s[2][256];
 int f;
 
-int p;
-int i = 1;
+void wcschg(wchar_t*, const wchar_t*, const wchar_t*);
 
-int WINAPI wWinMain(HINSTANCE hI, HINSTANCE hP, LPWSTR lpC, int nC)
-{
-	int l = wcslen(lpC);
-	p = 0;
-	for (; i < l; i++) {
-		if (lpC[i] == L'\\') {
-			i++;
-			switch (lpC[i]) {
-			case L'"':
-				break;
-			case L'n':
-				lpC[i] = L'\n';
-				break;
-			case L't':
-				lpC[i] = L'\t';
-				break;
-			}
-		}
-		else if (lpC[i] == L'"') break;
-		s[p] = lpC[i];
-		p++;
+int wmain(int argc, wchar_t* argv[]) {
+	if (argc != 4) return 1;
+
+	_setmode(_fileno(stdin), _O_WTEXT);
+
+	for (int i = 1; i <= 2; i++) {
+		wcschg(argv[i], L"\\n", L"\n");
+		wcschg(argv[i], L"\\t", L"\t");
 	}
 
-	i++;
-	for (; i < l; i++) {
-		if (lpC[i] == L'"') break;
-	}
+	swscanf_s(argv[3], L"%X", &f);
 
-	i++;
-	p = 0;
-	for (; i < l; i++) {
-		if (lpC[i] == L'\\') {
-			i++;
-			switch (lpC[i]) {
-			case L'"':
-				break;
-			case L'n':
-				lpC[i] = L'\n';
-				break;
-			case L't':
-				lpC[i] = L'\t';
-				break;
-			}
-		}
-		else if (lpC[i] == L'"') break;
-		t[p] = lpC[i];
-		p++;
-	}
-
-	i++;
-	for (; i < l; i++) {
-		if (lpC[i] != L' ') break;
-	}
-
-	swscanf_s(lpC + i, L"%X", &f);
-
-	MessageBox(NULL, s, t, f);
+	printf("%d", MessageBox(NULL, argv[1], argv[2], f));
 
 	return 0;
+}
+
+void wcschg(wchar_t* buf, const wchar_t* str1, const wchar_t* str2)
+{
+	wchar_t tmp[1024 + 1];
+	wchar_t* p;
+
+	while ((p = wcsstr(buf, str1)) != NULL) {
+		/* 見つからなくなるまで繰り返す
+			  pは旧文字列の先頭を指している */
+		*p = L'\0'; /* 元の文字列を旧文字列の直前で区切って */
+		p += wcslen(str1);  /* ポインタを旧文字列の次の文字へ */
+		wcscpy(tmp, p);             /* 旧文字列から後を保存 */
+		wcscat(buf, str2);  /* 新文字列をその後につなぎ */
+		wcscat(buf, tmp);   /* さらに残りをつなぐ */
+	}
 }
